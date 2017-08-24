862439153cbd97a0d7875ba8edd6ede944debc5a
Jorge Silva <jsilvab.externo@bancofalabella.cl>
2017-08-24 09:43:42 -0300

    Modificación de la clase Conexion

--- src/com/Conexion.java
+++ src/com/Conexion.java
@@ -1,222 +1,73 @@
 package com;
 
-import com.conexion.mysql.ConexionMySql;
-import com.conexion.oracle.ConexionOracle;
-import com.conexion.sqlserver.ConexionSqlServer;
-import com.exception.ExcepcionConexionNula;
-import com.exception.ExcepcionPassword;
-import com.exception.ExcepcionServidor;
-import com.exception.ExcepcionUsuario;
-import com.exception.ExcpcionSIDOracle;
+import com.exception.*;
 import java.sql.Connection;
-import java.sql.ResultSet;
+import java.sql.DriverManager;
 import java.sql.SQLException;
+
 
 /**
  * Clase principal manejadora de las conexiones de acceso a las bases de datos.
- *
  * @author Jorge Silva Borda
  */
-public class Conexion implements DatosConexion {
-
-    private String servidor, usuario, password, basedato;
-    private int puerto;
-    private Connection con;
-    private int TIPO;
-    private Object CONEXIONACTUAL;
+public class Conexion {
+    private String SERVIDOR, USUARIO, PASSWORD, BASEDATOS, SID;
+    private int PUERTO;
+    private Connection connection;
 
     //<editor-fold defaultstate="collapsed" desc="Constructores">
-    public Conexion() {
-
+    public Conexion(String SERVIDOR, String USUARIO, String PASSWORD, String BASEDATOS) {
+	this.SERVIDOR = SERVIDOR;
+	this.USUARIO = USUARIO;
+	this.PASSWORD = PASSWORD;
+	this.BASEDATOS = BASEDATOS;
     }
-
-    public Conexion(Connection conn) {
-	this.con = conn;
+    
+    public Conexion(String SERVIDOR, String USUARIO, String PASSWORD, String BASEDATOS, int PUERTO) {
+	this.SERVIDOR = SERVIDOR;
+	this.USUARIO = USUARIO;
+	this.PASSWORD = PASSWORD;
+	this.BASEDATOS = BASEDATOS;
+	this.PUERTO = PUERTO;
     }
-
-    /**
-     * Constructor con todos los datos para la conexión
-     *
-     * @param servidor
-     * @param usuario
-     * @param password
-     * @param basedato
-     */
-    public Conexion(String servidor, String usuario, String password, String basedato) {
-	this.servidor = servidor;
-	this.usuario = usuario;
-	this.password = password;
-	this.basedato = basedato;
-    }
-
-    public Conexion(String servidor, String usuario, String password, String basedato, Connection conn) {
-	this.servidor = servidor;
-	this.usuario = usuario;
-	this.password = password;
-	this.basedato = basedato;
-	this.con = conn;
+    
+    public Conexion(String SERVIDOR, String USUARIO, String PASSWORD, String BASEDATOS, String SID, int PUERTO) {
+	this.SERVIDOR = SERVIDOR;
+	this.USUARIO = USUARIO;
+	this.PASSWORD = PASSWORD;
+	this.BASEDATOS = BASEDATOS;
+	this.SID = SID;
+	this.PUERTO = PUERTO;
     }
 //</editor-fold>
-
-    @Override
-    public void activarConexion(int tipo) throws ExcepcionConexionNula, ExcepcionServidor, ExcepcionUsuario, ExcepcionPassword, ExcpcionSIDOracle {
-	this.TIPO = tipo;
-	if (validar()) {
-	    switch (TIPO) {
-		case ORACLE:
-		    throw new ExcpcionSIDOracle("No se puede generar una conexión a Oracle Database si no se ha proporcionado el SID de la conexión.");
-		case SQLSERVER:
-
-		case MYSQL:
-		default:
-	    }
-	} else {
-	    System.out.println("Error al validar los datos de la conexión.");
+    
+    public void abrirConexionOracle() throws ExcpcionSIDOracle, ExcepcionServidor, ExcepcionUsuario, ExcepcionPassword, ClassNotFoundException, SQLException{
+	if(SID.equals("")){
+	    throw new ExcpcionSIDOracle("No se ha definido el SID de la conexión.");
+	}else if(SID == null){
+	    throw new ExcpcionSIDOracle("No se ha inicializado el SID de la conexión.", new NullPointerException());
+	}else if(validar()){
+	    Class.forName("oracle.jdbc.driver.OracleDriver");
+	    this.connection = DriverManager.getConnection("jdbc:oracle:thin:@" + SERVIDOR + ":" + Integer.toString(PUERTO) + ":" + SID, USUARIO, PASSWORD);
 	}
     }
-
-    @Override
-    public void activarConexion(int tipo, String SID) throws ExcepcionConexionNula, ExcepcionServidor, ExcepcionUsuario, ExcepcionPassword, ExcpcionSIDOracle {
-	this.TIPO = tipo;
-	if (validar()) {
-	    switch (TIPO) {
-		case ORACLE:
-		    CONEXIONACTUAL = new ConexionOracle(getServidor(), getUsuario(), getBasedato(), getPassword(), getPuerto(), SID);
-		case SQLSERVER:
-
-		case MYSQL:
-
-		default:
-
-	    }
-	} else {
-	    System.out.println("Error al validar los datos de la conexión.");
-	}
-    }
-
-    public void abrirConexion() throws ExcpcionSIDOracle, ClassNotFoundException, SQLException {
-	if (CONEXIONACTUAL instanceof ConexionOracle) {
-	    ((ConexionOracle) CONEXIONACTUAL).abrirConexion();
-	} else if (CONEXIONACTUAL instanceof ConexionSqlServer) {
-
-	} else if (CONEXIONACTUAL instanceof ConexionMySql) {
-
-	}
-    }
-
-    public ResultSet ejecutarQuery(String query) throws SQLException {
-	if (CONEXIONACTUAL instanceof ConexionOracle) {
-	    return ((ConexionOracle) CONEXIONACTUAL).ejecutarQuery(query);
-	} else if (CONEXIONACTUAL instanceof ConexionSqlServer) {
-
-	} else if (CONEXIONACTUAL instanceof ConexionMySql) {
-
-	}
-	return null;
-    }
-
-    public ResultSet ejecutar(String query) throws SQLException {
-	if (CONEXIONACTUAL instanceof ConexionOracle) {
-	    ((ConexionOracle) CONEXIONACTUAL).ejecutar(query);
-	} else if (CONEXIONACTUAL instanceof ConexionSqlServer) {
-
-	} else if (CONEXIONACTUAL instanceof ConexionMySql) {
-
-	}
-	return null;
-    }
-
-    public void cerrar() throws SQLException {
-	if (CONEXIONACTUAL instanceof ConexionOracle) {
-	    ((ConexionOracle) CONEXIONACTUAL).cerrarConexion();
-	} else if (CONEXIONACTUAL instanceof ConexionSqlServer) {
-
-	} else if (CONEXIONACTUAL instanceof ConexionMySql) {
-
-	}
-    }
-
-    private boolean validar() throws ExcepcionConexionNula, ExcepcionServidor, ExcepcionUsuario, ExcepcionPassword {
-	if (this.con == null) {
-	    throw new ExcepcionConexionNula("No se ha inicializado la variable de la conexión [Valor null].", new NullPointerException());
-	} else if (servidor.equals("")) {
-	    throw new ExcepcionServidor("No se ha definido el servidor de destino.");
-	} else if (servidor == null) {
-	    throw new ExcepcionServidor("No se ha inicializado el servidor de destino.", new NullPointerException());
-	} else if (usuario.equals("")) {
-	    throw new ExcepcionUsuario("No se ha definido el usuario de destino.");
-	} else if (usuario == null) {
-	    throw new ExcepcionUsuario("No se ha inicializado el usuario de destino.", new NullPointerException());
-	} else if (password == null) {
-	    throw new ExcepcionPassword("No se ha inicializado el password  de la base de datos.", new NullPointerException());
+    
+    public boolean validar() throws ExcepcionServidor, ExcepcionUsuario, ExcepcionPassword{
+	if(SERVIDOR.equals("")){
+	    throw new ExcepcionServidor("El nombre del servidor de destino no puede ser vacío.");
+	}else if(SERVIDOR == null){
+	    throw new ExcepcionServidor("El nombre del servidor de destino no puede ser nulo.", new NullPointerException());
+	}else if(USUARIO.equals("")){
+	    throw new ExcepcionUsuario("El nombre de usuario no puede ser vacío.");
+	}else if(USUARIO == null){
+	    throw new ExcepcionUsuario("El nombre de usuario no puede ser nulo.", new NullPointerException());
+	}else if(PASSWORD.equals("")){
+	    throw new ExcepcionPassword("La contraseña no puede ser vacía.");
+	}else if(PASSWORD == null){
+	    throw new ExcepcionPassword("La contraseña no puede ser nula.", new NullPointerException());
 	}
 	return true;
     }
-
-    @Override
-    public void probarConexion() {
-	
-    }
-
-    //<editor-fold defaultstate="collapsed" desc="Accesores">
-    @Override
-    public String getServidor() {
-	return servidor;
-    }
-
-    @Override
-    public void setServidor(String servidor) {
-	this.servidor = servidor;
-    }
-
-    @Override
-    public String getUsuario() {
-	return usuario;
-    }
-
-    @Override
-    public void setUsuario(String usuario) {
-	this.usuario = usuario;
-    }
-
-    @Override
-    public String getPassword() {
-	return password;
-    }
-
-    @Override
-    public void setPassword(String password) {
-	this.password = password;
-    }
-
-    @Override
-    public String getBasedato() {
-	return basedato;
-    }
-
-    @Override
-    public void setBasedato(String basedato) {
-	this.basedato = basedato;
-    }
-
-    @Override
-    public Connection getCon() {
-	return con;
-    }
-
-    @Override
-    public void setCon(Connection con) {
-	this.con = con;
-    }
-
-    @Override
-    public void setPuerto(int puerto) {
-	this.puerto = puerto;
-    }
-
-    @Override
-    public int getPuerto() {
-	return puerto;
-    }
-//</editor-fold>
+    
+    
 }
